30ceee94ef731faf3902dc3dcdcc14a9945d896c
Mayank Dungarwal <73541497+mayankd07@users.noreply.github.com>
2021-07-03 16:49:03 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+$('.slider').slick({
+    arrows:false,
+    dots: true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots' 
+});
+
+$('#link1').click(function(){
+    mobileNav.classList.remove('open');
+});
+$('#link2').click(function(){
+    mobileNav.classList.remove('open');
+});
+$('#link3').click(function(){
+    mobileNav.classList.remove('open');
+});
+$('#link4').click(function(){
+    mobileNav.classList.remove('open');
+});
+$('#link5').click(function(){
+    mobileNav.classList.remove('open');
+});
+$('#link6').click(function(){
+    mobileNav.classList.remove('open');
+});
+$('#link7').click(function(){
+    mobileNav.classList.remove('open');
+});
+
+
+let hamburger = document.querySelector('.hamburger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+});
+
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+
+});
